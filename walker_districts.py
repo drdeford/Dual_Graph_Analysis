75a0472e445bfa103de06a57fb36748d6f5c7708
Daryl DeFord <daryl.r.deford@gmail.com>
2019-01-30 10:38:49 -0500

    Constructs dual graph and randomizes move order

--- walker_districts.py
+++ walker_districts.py
@@ -6,12 +6,12 @@ Created on Tue Jan 22 07:56:10 2019
 """
 
 import networkx as nx
-from random import randint, random, choice
+from random import randint, random, choice, shuffle
 import matplotlib.pyplot as plt
 
-n = 10#grid size
-m = 10#grid size
-k = 8# number of walkers
+n = 100#grid size
+m = 100#grid size
+k = 1000# number of walkers
 
 
 grid = nx.grid_graph([n,m])
@@ -28,26 +28,40 @@ for i in range(k):
     unassigned.remove(walkers[-1])
     cdict[walkers[-1]]=i+1
     
-    
+plt.figure()
 nx.draw(grid,pos= {x:x for x in grid.nodes()},node_color=[cdict[x] for x in grid.nodes()],node_size=600,cmap='tab20')#cmap=plt.cm.jet,label=True)
-
+plt.title("Initial Walkers")
 
 
 move = 0
 
 while unassigned:
+    order = list(range(k))
+    shuffle(order)
 	
-    for i in range(k):
+    for i in order:
+        old=walkers[i]
+        #print(old)
         walkers[i]=choice(list(grid.neighbors(walkers[i])))
+        #print(walkers[i])
         if walkers[i] in unassigned:
             unassigned.remove(walkers[i])
             cdict[walkers[i]]=i+1
+            grid = nx.contracted_nodes(grid, walkers[i], old, self_loops=False)
+        else:
+            walkers[i]=old
     #plt.figure()
     #nx.draw(grid,pos= {x:x for x in grid.nodes()},node_color=[cdict[x] for x in grid.nodes()],node_size=600,cmap='tab20')#cmap=plt.cm.jet,label=True)
 
             
 plt.figure()
-nx.draw(grid,pos= {x:x for x in grid.nodes()},node_color=[cdict[x] for x in grid.nodes()],node_size=600,cmap='tab20')#cmap=plt.cm.jet,label=True)
+nx.draw(grid,pos= {x:x for x in grid.nodes()},node_color=['r' for x in grid.nodes()],label=True)
+plt.title("Dual Graph")
 
-# add dual graph consturction here!
+grid2 = nx.grid_graph([n,m])
+plt.figure()
+nx.draw(grid2,pos= {x:x for x in grid2.nodes()},node_color=[cdict[x] for x in grid2.nodes()],node_size=400,cmap='tab20')#cmap=plt.cm.jet,label=True)
+plt.title("Full Partition")
+
+
         
